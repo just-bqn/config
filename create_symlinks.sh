eaf2683fdaa6d3c83815b126ad1d8e22fc1aff81
just-bqn <self.bqnguyen@gmail.com>
2024-03-13 20:23:35 +0700

    Some commit.

---
+++ create_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+config_directory=/home/$USER/config
+
+ln -s $config_directory/hypr/ /home/$USER/.config/hypr
+ln -s $config_directory/kitty/ /home/$USER/.config/kitty
+ln -s $config_directory/pacdef/ /home/$USER/.config/pacdef
+ln -s $config_directory/rofi/ /home/$USER/.config/rofi
+ln -s $config_directory/waybar/ /home/$USER/.config/waybar
