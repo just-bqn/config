83318cca62408a61b47aa93a34ebee1b2a41de39
just-bqn <self.bqnguyen@gmail.com>
2024-03-16 00:15:45 +0700

    Some commit.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,10 +1,9 @@
 #!/bin/fish
 
-config_directory=/home/$USER/config
+set config_directory /home/$USER/config
 
-ln -s $config_directory/fish/ /home/$USER/.config/fish
-ln -s $config_directory/hypr/ /home/$USER/.config/hypr
-ln -s $config_directory/kitty/ /home/$USER/.config/kitty
-ln -s $config_directory/pacdef/ /home/$USER/.config/pacdef
-ln -s $config_directory/rofi/ /home/$USER/.config/rofi
-ln -s $config_directory/waybar/ /home/$USER/.config/waybar
+for dir in fish hypr kitty nvim pacdef rofi waybar
+  if not test -e /home/$USER/.config/$dir
+    ln -s $config_directory/$dir/ /home/$USER/.config/$dir
+  end
+end
